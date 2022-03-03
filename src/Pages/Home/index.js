7c42a682587f19b2fe63cee3c38cd7ad66d8da31
Gabriel-025 <78861515+Gabriel-025@users.noreply.github.com>
2022-03-03 14:20:27 -0300

    mesagem

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { Styledbtn, StyledMain} from './styles';
 import api from '../../services/api';
 import 'antd/dist/antd.css';
