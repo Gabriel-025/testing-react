e4123a8e53842543ac315c0689b94a1710250101
Gabriel-025 <78861515+Gabriel-025@users.noreply.github.com>
2022-05-05 10:07:45 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.sprinta.com.br/event_service/event/findbyuser/1513940438cs6HGu7O',
+  baseURL: 'secret_api',
 });
 export default api;
